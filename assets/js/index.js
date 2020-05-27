cfb739e2ade4c2183d295718b30cd22ca8ae9708
Ahmed gaafer <ahmedgaafer@yahoo.com>
2020-05-27 13:45:13 +0200

    updating howler

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,12 +5,5 @@ window.onload = () => {
     document.getElementById('snake-logo').classList.toggle('shake-bottom')
   }, 3000)
 
-  document.querySelectorAll('button').forEach(btn => {
-    btn.addEventListener('mouseover', () => {
-      new Howl({
-        src: ['assets/sounds/hover.mp3']
-      }).play()
-    })
-  })
 }
 
