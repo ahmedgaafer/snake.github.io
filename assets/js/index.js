2aff00d6c2bb50117681e4c1ed71f389f6e7e542
Ahmed gaafer <ahmedgaafer@yahoo.com>
2020-05-27 13:30:01 +0200

    init

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+
+window.onload = () => {
+
+  setInterval(() => {
+    document.getElementById('snake-logo').classList.toggle('shake-bottom')
+  }, 3000)
+
+  document.querySelectorAll('button').forEach(btn => {
+    btn.addEventListener('mouseover', () => {
+      new Howl({
+        src: ['../sounds/hover.mp3']
+      }).play()
+    })
+  })
+}
+
